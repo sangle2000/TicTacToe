3102b3cd30be302f3f6c642dca2148bffdc8e2af
sangle2000 <thanhsang22440011@gmail.com>
2024-04-21 10:05:50 +0700

    Add project files.

---
+++ Assets/Scenes/Scripts/Board.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [Header("Input Settings: ")]
+    // [SerializeField] private Layer
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Scripts/Box.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public int index;
+    public Mark mark;
+    public bool isMarked;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        index = transform.GetSiblingIndex();
+        mark = Mark.None;
+        isMarked = false;
+    }
+
+    public void SetAsMarked(Sprite sprite, Mark mark, Color color){
+        isMarked = true;
+        this.mark = mark;
+
+        spriteRenderer.color = color;
+        spriteRenderer.sprite = sprite;
+
+        // Disable the CircleConllider2D (to avoid marking it twice)
+        GetComponent<CircleConllider2D>().enable = false;
+
+    }
+}
